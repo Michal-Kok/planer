74e92dd41652faeb7bf3d1c2ab1dbc8dca58fd60
Michal-Kok <pukkix17@gmail.com>
2020-11-03 23:07:38 +0100

    Updates

---
+++ precache-manifest.a73ab88674270bce0a8769e16f92831d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b129dcc0ef78cbcf164a679005be9c",
+    "url": "/Michal-Kok/ToDoList/index.html"
+  },
+  {
+    "revision": "6f6a177745cf3c631fc2",
+    "url": "/Michal-Kok/ToDoList/static/css/main.be59a95d.chunk.css"
+  },
+  {
+    "revision": "1f8d67b7a19c322c48c1",
+    "url": "/Michal-Kok/ToDoList/static/js/2.57cd4440.chunk.js"
+  },
+  {
+    "revision": "e9802b9a2325a939eb66bf2fa755fb0f",
+    "url": "/Michal-Kok/ToDoList/static/js/2.57cd4440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6a177745cf3c631fc2",
+    "url": "/Michal-Kok/ToDoList/static/js/main.e3ca29d4.chunk.js"
+  },
+  {
+    "revision": "6d06af5e118090f7bb73",
+    "url": "/Michal-Kok/ToDoList/static/js/runtime-main.525b5f48.js"
+  },
+  {
+    "revision": "59a274d87c8b61d5e644c4d127ee3002",
+    "url": "/Michal-Kok/ToDoList/static/media/TitleIcon.59a274d8.svg"
+  },
+  {
+    "revision": "8606d9941394cf100800482553fbd32a",
+    "url": "/Michal-Kok/ToDoList/static/media/emptiness.8606d994.svg"
+  }
+]);
